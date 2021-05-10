3eaa8ee25482bf6638715a7ad9a464ad1f2d8a6e
yolandagb <yolandagarciabarranco@hotmail.com>
2021-05-10 11:51:48 +0000

    me falta la función

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,51 @@
 import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import { PropTypes } from "prop-types";
+import "../../styles/home.scss";
 
 //create your first component
-export function Home() {
+export function Home(props) {
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="simple-counter-container">
+			<div className="clock">
+				<i className="far fa-clock"></i>
+			</div>
+			<div className="five">{props.digitFive % 10}</div>
+			<div className="four">{props.digitFour % 10}</div>
+			<div className="three">{props.digitTree % 10}</div>
+			<div className="two">{props.digitTwo % 10}</div>
+			<div className="one">{props.digitOne % 10}</div>
+			<div className="heart">
+				<i className="fas fa-heart"></i>
+			</div>
 		</div>
 	);
 }
+Home.propTypes = {
+	digitFive: PropTypes.number,
+	digitFour: PropTypes.number,
+	digitTree: PropTypes.number,
+	digitTwo: PropTypes.number,
+	digitOne: PropTypes.number
+};
+
+// let counter = 0;
+// setInterval(function() {
+// 	const five = Math.floor(counter / 10000);
+// 	const four = Math.floor(counter / 1000);
+// 	const three = Math.floor(counter / 100);
+// 	const two = Math.floor(counter / 10);
+// 	const one = Math.floor(counter / 1);
+//     console.log(five, four, three, two, one);
+// }
+// 	counter++;
+// 	ReactDOM.render(
+// 		<SimpleCounter
+// 			digitOne={one}
+// 			digitTwo={two}
+// 			digitTree={three}
+// 			digitFour={four}
+// 			digitFive={five}
+// 		/>,
+// 		document.querySelector("#app")
+// 	);
+// }, 1000);
