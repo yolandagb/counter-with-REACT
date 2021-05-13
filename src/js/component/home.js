cb02206b405c9428d2e1f39a55e8687e200ecdc3
yolandagb <yolandagarciabarranco@hotmail.com>
2021-05-13 11:01:13 +0000

    done!

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import { PropTypes } from "prop-types";
 import "../../styles/home.scss";
 
@@ -14,9 +15,6 @@ export function Home(props) {
 			<div className="three">{props.digitTree % 10}</div>
 			<div className="two">{props.digitTwo % 10}</div>
 			<div className="one">{props.digitOne % 10}</div>
-			<div className="heart">
-				<i className="fas fa-heart"></i>
-			</div>
 		</div>
 	);
 }
@@ -28,24 +26,24 @@ Home.propTypes = {
 	digitOne: PropTypes.number
 };
 
-// let counter = 0;
-// setInterval(function() {
-// 	const five = Math.floor(counter / 10000);
-// 	const four = Math.floor(counter / 1000);
-// 	const three = Math.floor(counter / 100);
-// 	const two = Math.floor(counter / 10);
-// 	const one = Math.floor(counter / 1);
-//     console.log(five, four, three, two, one);
-// }
-// 	counter++;
-// 	ReactDOM.render(
-// 		<SimpleCounter
-// 			digitOne={one}
-// 			digitTwo={two}
-// 			digitTree={three}
-// 			digitFour={four}
-// 			digitFive={five}
-// 		/>,
-// 		document.querySelector("#app")
-// 	);
-// }, 1000);
+let counter = 0;
+setInterval(function() {
+	const five = Math.floor(counter / 10000);
+	const four = Math.floor(counter / 1000);
+	const three = Math.floor(counter / 100);
+	const two = Math.floor(counter / 10);
+	const one = Math.floor(counter / 1);
+	console.log(five, four, three, two, one);
+
+	counter++;
+	ReactDOM.render(
+		<Home
+			digitOne={one}
+			digitTwo={two}
+			digitTree={three}
+			digitFour={four}
+			digitFive={five}
+		/>,
+		document.querySelector("#app")
+	);
+}, 1000);
